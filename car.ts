2224e5730106882d5e4a9a3395cf90b9c7377ea9
ElecFreaks <services@elecfreaks.com>
2018-12-18 15:28:13 +0800

    Update car.ts

--- car.ts
+++ car.ts
@@ -1,243 +1,109 @@
+
 /**
  * Use this file to define custom functions and blocks.
  * Read more at https://makecode.microbit.org/blocks/custom
  */
 
 
-//% weight=0 color=#0fbc11  icon="\uf207" block="ringbit车"
-namespace ringbitcar {
-    
-    export enum TrackingStateType {
-        //% block="● ●" 
-        Tracking_State_0 = 0,
 
-        //% block="● ◌" 
-        Tracking_State_1 = 1,
 
-        //% block="◌ ●" 
-        Tracking_State_2 = 2,
+//% weight=0 color=#0fbc11  icon="\uf11b" block="Joystickbit"
+namespace joystickbit {
 
-        //% block="◌ ◌"
-        Tracking_State_3 = 3
+    export enum JoystickBitPin {
+        //% block="C"
+        P12 = DAL.MICROBIT_ID_IO_P12,
+        //% block="D"
+        P13 = DAL.MICROBIT_ID_IO_P13,
+        //% block="E"
+        P14 = DAL.MICROBIT_ID_IO_P14,
+        //% block="F"
+        P15 = DAL.MICROBIT_ID_IO_P15
+    }
+
+    export enum rockerType {
+        //% block="X"
+        X,
+        //% block="Y"
+        Y
     }
 
 
-    export enum Distance_Unit {
-        //% block="mm" enumval=0
-        Distance_Unit_mm,
-
-        //% block="cm" enumval=1
-        Distance_Unit_cm,
-
-        //% block="inch" enumval=2
-        Distance_Unit_inch,
+    export enum ButtonType {
+        //% block="pressed"
+        down = PulseValue.High,
+        //% block="released"
+        up = PulseValue.Low
     }
-
-    let pin_left_wheel = AnalogPin.P1
-    let pin_right_wheel = AnalogPin.P2
-
 
     /**
-    * TODO: initialization ring:bit car
-    * @param left describe parameter here, eg: AnalogPin.P1
-    * @param right describe parameter here, eg: AnalogPin.P2
+    * initialization joystick:bit
     */
-    //% weight=10
-    //% blockId=ringbitcar_init block="set left wheel at pin %left|right wheel at pin %right"
-    export function init_wheel(left: AnalogPin, right: AnalogPin): void {
-        // Add code here
-        pin_left_wheel = left
-        pin_right_wheel = right
-
+    //% blockId=initJoystickBit block="initialization joystick:bit"
+    export function initJoystickBit(): void {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.setPull(DigitalPin.P12, PinPullMode.PullUp)
+        pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
+        pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
+        pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
+        pins.digitalWritePin(DigitalPin.P16, 1)
     }
 
-
-
     /**
-    * TODO: full speed move forward
+    * get Button
     */
-    //% weight=9
-    //% blockId=ringbitcar_forward block="go straight at full speed"
-    export function forward(): void {
-        // Add code here
-
-        pins.servoSetPulse(pin_left_wheel, 2400)
-        pins.servoSetPulse(pin_right_wheel, 600)
-
+    //% blockId=getButton block="button %button is pressed"
+    export function getButton(button: JoystickBitPin): boolean {
+        return (pins.digitalReadPin(<number>button) == 0 ? true : false)
     }
 
 
 
     /**
-    * TODO: full speed move back
+    * Registers code to run when a joystick:bit event is detected.
     */
-    //% weight=8
-    //% blockId=ringbitcar_back block="reverse at full speed"
-    export function back(): void {
-        // Add code here
-
-        pins.servoSetPulse(pin_left_wheel, 600)
-        pins.servoSetPulse(pin_right_wheel, 2400)
-
+    //% blockId=onButtonEvent block="on button %button|is %event" blockExternalInputs=false
+    export function onButtonEvent(button: JoystickBitPin, event: ButtonType, handler: Action): void {
+        pins.onPulsed(<number>button, <number>event, handler);
     }
 
 
 
     /**
-    * TODO: full speed turn left
+    * get rocker value
+    * @param rocker describe parameter here, eg: 1
     */
-    //% weight=7
-    //% blockId=ringbitcar_left block="turn left at full speed"
-    export function turnleft(): void {
-        // Add code here
-
-        pins.servoSetPulse(pin_left_wheel, 600)
-        pins.servoSetPulse(pin_right_wheel, 600)
-
-    }
-
-
-    /**
-    * TODO: full speed turn right
-    */
-    //% weight=6
-    //% blockId=ringbitcar_right block="turn right at full speed"
-    export function turnright(): void {
-        // Add code here
-
-        pins.servoSetPulse(pin_left_wheel, 2400)
-        pins.servoSetPulse(pin_right_wheel, 2400)
-
-    }
-
-
-    /**
-    * TODO: stop
-    */
-    //% weight=5
-    //% blockId=ringbitcar_brake block="brake"
-    export function brake(): void {
-        // Add code here
-
-        //pins.servoSetPulse(pin_left_wheel, 1500)
-        //pins.servoSetPulse(pin_right_wheel, 1500)
-        pins.digitalWritePin(<number>pin_left_wheel, 0)
-        pins.digitalWritePin(<number>pin_right_wheel, 0)
-
-    }
-
-
-
-    /**
-    * TODO: self setting speed
-    * @param m the m from -100 (min) to 100 (max), eg:0
-    * @param n the n from -100 (min) to 100 (max), eg:0
-    */
-    //% weight=4
-    //% blockId=ringbitcar_freestyle block="set left wheel speed %m| right wheel speed %n"
-    //% m.min=-100 m.max=100
-    //% n.min=-100 n.max=100
-    export function freestyle(m: number, n: number): void {
-        // Add code here
-
-        if (m > 0) {
-            pins.servoSetPulse(pin_left_wheel, 1600 + m * 8)
-        } else if (m < 0) {
-            pins.servoSetPulse(pin_left_wheel, 1400 + m * 8)
-        } else pins.servoSetPulse(pin_left_wheel, 1500)
-
-
-        if (n > 0) {
-            pins.servoSetPulse(pin_right_wheel, 1400 - n * 8)
-        } else if (n < 0) {
-            pins.servoSetPulse(pin_right_wheel, 1600 - n * 8)
-        } else pins.servoSetPulse(pin_right_wheel, 1500)
-
-    }
-
-
-
-    /**
-    * TODO: line following
-    */
-    //% weight=10
-    //% advanced=true
-    //% blockId=ringbitcar_tracking block="tracking state is %state"
-    export function tracking(state: TrackingStateType): boolean {
-        let sensor_pin = AnalogPin.P0
-
-        if (pin_left_wheel != AnalogPin.P1 && pin_right_wheel != AnalogPin.P1) {
-            sensor_pin = AnalogPin.P1
-        } else if (pin_left_wheel != AnalogPin.P2 && pin_right_wheel != AnalogPin.P2) {
-            sensor_pin = AnalogPin.P2
+    //% blockId=getRockerValue block="rocker value of %rocker"
+    export function getRockerValue(rocker: rockerType): number {
+        switch (rocker) {
+            case rockerType.X: return pins.analogReadPin(AnalogPin.P1);
+            case rockerType.Y: return pins.analogReadPin(AnalogPin.P2);
+            default: return 0;
         }
-
-        let i = pins.analogReadPin(sensor_pin)
-
-        if (i < 100 && state == 0) {
-            return true;
-        } else if (i > 100 && i < 200 && state == 1) {
-            return true;
-        } else if (i > 200 && i < 300 && state == 2) {
-            return true;
-        } else if (i > 300 && i < 400 && state == 3) {
-            return true;
-        } else return false;
-
     }
+
 
 
 
     /**
-    * TODO: get ultrasonic distance
+    * vibration motor
+    * @param time describe parameter here, eg: 100
     */
-    //% weight=9
-    //% advanced=true
-    //% blockId=ringbitcar_sonarbit block="ultrasonic distance in unit %distance_unit"
-    export function ringbitcar_sonarbit(distance_unit: Distance_Unit): number {
-
-        let sensor_pin = AnalogPin.P0
-
-        if (pin_left_wheel != AnalogPin.P1 && pin_right_wheel != AnalogPin.P1) {
-            sensor_pin = AnalogPin.P1
-        } else if (pin_left_wheel != AnalogPin.P2 && pin_right_wheel != AnalogPin.P2) {
-            sensor_pin = AnalogPin.P2
-        }
-
-        // send pulse
-        pins.setPull(<number>sensor_pin, PinPullMode.PullNone)
-        pins.digitalWritePin(<number>sensor_pin, 0)
-        control.waitMicros(2)
-        pins.digitalWritePin(<number>sensor_pin, 1)
-        control.waitMicros(10)
-        pins.digitalWritePin(<number>sensor_pin, 0)
-
-        // read pulse
-        let d = pins.pulseIn(<number>sensor_pin, PulseValue.High, 23000)  // 8 / 340 = 
-        let distance = d * 10 * 5 / 3 / 58
-
-        if (distance > 4000) distance = 0
-
-        switch (distance_unit) {
-            case 0:
-                return distance //mm
-                break
-            case 1:
-                return distance / 10  //cm
-                break
-            case 2:
-                return distance / 25  //inch
-                break
-            default:
-                return 0
-
-        }
-
+    //% blockId=Vibration_Motor block="motor vibrate for %time ms"
+    export function Vibration_Motor(time: number): void {
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        basic.pause(time)
+        pins.digitalWritePin(DigitalPin.P16, 1)
     }
+
+
+
+
+
 
 
 
 
 
 }
-
+ 
